5fcd737e06930225a3b8d46b5699883c46bbabac
Oh9yu <kog6148@gmail.com>
2024-02-04 17:38:02 +0900

    Feature : main screen

--- business_card/lib/main_screen.dart
+++ business_card/lib/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -10,6 +12,39 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          leading: Icon(
+            Icons.person,
+            color: Colors.black,
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            'Flutter Developer',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          )),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.all(20),
+                width: double.infinity,
+                height: 160,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    'assets/profile.jpeg',
+                    fit: BoxFit.cover,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
