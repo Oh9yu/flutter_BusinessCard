dcd6d740b525061db4f1985970f9018a53ea30bb
Oh9yu <kog6148@gmail.com>
2024-02-05 16:14:38 +0900

    Add : TextField

--- business_card/lib/main_screen.dart
+++ business_card/lib/main_screen.dart
@@ -10,6 +10,8 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  TextEditingController introduceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +32,9 @@ class _MainScreenState extends State<MainScreen> {
           )),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // image section
             Container(
                 margin: EdgeInsets.all(20),
                 width: double.infinity,
@@ -41,7 +45,109 @@ class _MainScreenState extends State<MainScreen> {
                     'assets/profile.jpeg',
                     fit: BoxFit.cover,
                   ),
-                ))
+                )),
+            // text section
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    child: Text(
+                      '이름',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text('권오규'),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    child: Text(
+                      '나이',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text('27'),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    child: Text(
+                      '취미',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text('게임'),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    child: Text(
+                      '직업',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text('프론트엔드'),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    child: Text(
+                      '전공',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text('조리학'),
+                ],
+              ),
+            ),
+            // input section
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 20),
+              child: Text(
+                '자기소개',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: TextField(
+                maxLines: 6,
+                controller: introduceController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Colors.blueGrey, width: 2.0),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
